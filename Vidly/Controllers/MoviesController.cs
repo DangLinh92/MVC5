53ff13f4661e02daa8e962742133e2fa1c5fbb86
DangLinh92 <hoamoclan.3192@gmail.com>
2020-03-20 22:26:45 +0700

    mvc5

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -13,8 +13,19 @@ namespace Vidly.Controllers
         public ActionResult Random()
         {
             var movie = new Movie() {Name = "Shrek"};
-
+            ViewData["Movie"] = movie;
+            var rs = new ViewResult();
             return View(movie);
+        }
+
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month);
+        }
+
+        public ActionResult New()
+        {
+            return View();
         }
     }
 }
--- Vidly/Models/Movie.cs
+++ Vidly/Models/Movie.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,6 +10,10 @@ namespace Vidly.Models
     public class Movie
     {
         public int Id { get; set; }
+
+        [Required]
+        [MaxLength(10)]
+        [Display(Name = "Ho Ten")]
         public string Name { get; set; }
     }
 }
