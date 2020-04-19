772ed1d44749e550fa848037122307fc9fb2303d
DangLinh92 <hoamoclan.3192@gmail.com>
2020-04-20 00:05:03 +0700

    get role for user

--- Redmine/Controllers/RedmineHomeController.cs
+++ Redmine/Controllers/RedmineHomeController.cs
@@ -1,8 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Redmine.Models;
 
 namespace Redmine.Controllers
 {
@@ -12,6 +17,19 @@ namespace Redmine.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        public string GetRoleOfUser(string userId)
+        {
+            string role = "";
+            using (ApplicationDbContext context = new ApplicationDbContext())
+            {
+                var userStore = new UserStore<ApplicationUser>(context);
+                var userManager = new UserManager<ApplicationUser>(userStore);
+                var roles = userManager.GetRoles(userId);
+                role = roles.Count > 0 ?roles[0]:"";
+            }
+            return role;
         }
     }
 }
